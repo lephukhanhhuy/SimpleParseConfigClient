56befcfb0a962617b7bc80bb9a6e3f1ce81cbe97
Le Huy <lehuy@Les-MacBook-Air.local>
2015-04-16 14:04:56 +0700

    Initial Commit

---
+++ SimpleParseConfigClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleParseConfigClient
+//
+//  Created by Le Huy on 4/16/15.
+//  Copyright (c) 2015 Huy Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleParseConfigClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleParseConfigClient
+//
+//  Created by Le Huy on 4/16/15.
+//  Copyright (c) 2015 Huy Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
